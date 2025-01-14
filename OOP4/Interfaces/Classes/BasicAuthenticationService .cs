68ae0d5cc08c04df7776475285010e0d584e6879
Abdelrahmany20 <abdelrahman.yasser.m17@gmail.com>
2025-01-14 05:25:47 +0200

    Q02 P2

---
+++ OOP4/Interfaces/Classes/BasicAuthenticationService .cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP4.Interfaces.Classes
+{
+    internal class BasicAuthenticationService : IAuthenticationService
+    {
+        public bool AuthenticateUser(string UserName,string PW)
+        {
+            if (UserName == "Alex" && PW == "123")
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool AuthenticateUser()
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
+        public bool AuthorizeUser(string UserName, string Role)
+        {
+           if(UserName == "Alex" && Role == "Alex")
+            {
+                return true;
+            }
+           return false;
+                
+        }
+
+        public bool AuthorizeUser()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- OOP4/Program.cs
+++ OOP4/Program.cs
@@ -11,17 +11,60 @@ namespace OOP4
 
             #region Q01
 
-            Circle circle =new Circle(10);
+            //Circle circle =new Circle(10);
 
-            circle.DisplayShapeInfo();
+            //circle.DisplayShapeInfo();
 
 
-            IRectangle rectangle = new Rectangle(5, 20);
-            rectangle.DisplayShapeInfo();
+            //IRectangle rectangle = new Rectangle(5, 20);
+            //rectangle.DisplayShapeInfo();
             #endregion
 
 
+            #region Q02
+
+            BasicAuthenticationService service = new BasicAuthenticationService();
+
+            Console.WriteLine("Enter username:");
+            string username = Console.ReadLine();
+
+            Console.WriteLine("Enter password:");
+            string password = Console.ReadLine();
+
+            bool Authenticated= service.AuthenticateUser(username,password);
+            if (Authenticated)
+            {
+                Console.WriteLine("welcome");
+
+
+            }
+            else
+            {
+                Console.WriteLine("faild AuthenticateUser");
+                return;
+            }
+
+
+
+
+
+            Console.WriteLine("Enter role:");
+            string role = Console.ReadLine();
+
+            bool Authorized = service.AuthorizeUser(username,role);
+            if (Authorized)
+            {
+                Console.WriteLine("welcome");
+            }
+            else
+            {
+                Console.WriteLine("faild AuthorizeUser");
+            }
+
             #endregion
+
+
+                #endregion
         }
     }
 }
